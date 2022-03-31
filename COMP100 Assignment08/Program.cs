074ad0a3ff955f15f88edcf23bf5ac5edcc8e6e2
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-03-30 20:50:54 -0400

    Q4

--- COMP100 Assignment08/Program.cs
+++ COMP100 Assignment08/Program.cs
@@ -8,11 +8,11 @@ namespace COMP100_Assignment08
 {
     internal class Program
     {
-        static void Print(string[] strArray)
+        static void Print(char[] charArray)
         {
-            foreach (var str in strArray)
+            foreach (var cha in charArray)
             {
-                Console.Write(str + " ");
+                Console.Write(cha + " ");
             }
         }
         static void Main(string[] args)
@@ -21,8 +21,12 @@ namespace COMP100_Assignment08
             int[] numbers = { 0, 2, 3, 5, 7, 1, 1, 2, 5, 6, 7, 2, 5, 2 };
             string[] poem = "mary had a little lamb its fleece was white as snow".Split();
 
-            Print(poem);
+            Print(letters);
+            Array.Reverse(letters);
+            Console.WriteLine();
+            Print(letters);
             Console.ReadLine();
         }
+
     }
 }
