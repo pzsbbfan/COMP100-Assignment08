005c1a6df3ea01e3eeb920a6823cbcc7d3bdc719
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-03-30 20:45:37 -0400

    Q1

--- COMP100 Assignment08/Program.cs
+++ COMP100 Assignment08/Program.cs
@@ -8,8 +8,21 @@ namespace COMP100_Assignment08
 {
     internal class Program
     {
+        static void Print(char[] charArray)
+        {
+            foreach (var cha in charArray)
+            {
+                Console.Write(cha + " ");
+            }
+        }
         static void Main(string[] args)
         {
+            char[] letters = "the quick brown fox jumps over the lazy dog".ToCharArray();
+            int[] numbers = { 0, 2, 3, 5, 7, 1, 1, 2, 5, 6, 7, 2, 5, 2 };
+            string[] poem = "mary had a little lamb its fleece was white as snow".Split();
+
+            Print(letters);
+            Console.ReadLine();
         }
     }
 }
