db87c2fe2456298bd1b65b8486d5b9b4766edcf2
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-03-30 22:01:53 -0400

    Q9

--- COMP100 Assignment08/Program.cs
+++ COMP100 Assignment08/Program.cs
@@ -9,12 +9,12 @@ namespace COMP100_Assignment08
     internal class Program
     {
         static Random random = new Random();
-        static int[] func(int length)
+        static char[] func(int length)
         {
-            int[] myArray = new int[length];
+            char[] myArray = new char[length];
             for (int i = 0; i < myArray.Length; i++)
             {
-                myArray[i] = random.Next(100, 200);
+                myArray[i] =(char)random.Next('A','Z');
             }
             
             return myArray;
@@ -25,15 +25,12 @@ namespace COMP100_Assignment08
             int[] numbers = { 0, 2, 3, 5, 7, 1, 1, 2, 5, 6, 7, 2, 5, 2 };
             string[] poem = "mary had a little lamb its fleece was white as snow".Split();
 
-            int[] a = func(5);
+            char[] a = func(5);
             Console.WriteLine();
             Console.WriteLine(a);
             Console.WriteLine();
             Console.WriteLine("All elements in this variable: " + String.Join(" ",a));
             Console.ReadLine();
         }
-
-
-
     }
 }
